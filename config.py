ac1dcc7df57ca7055b5fa10a7e17eae2b1e99f1c
waynewu6250 <waynewu6250@gmail.com>
2019-10-18 23:13:14 -0400

    Add

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 class Config:
 
+    data_path = "data/raw_data.pkl" #"Multi-intent-dialoguer/data/raw_data.pkl"
+    model_path = None #"bert_test.pth"
+
+    maxlen = 256
+    batch_size = 16
+    epochs = 25
+    learning_rate_bert = 1e-3
+    learning_rate_classifier = 1e-3
+
+
 
 
 opt = Config()
