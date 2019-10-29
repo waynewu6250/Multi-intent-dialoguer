246f708e4384df1bf250cd7b0ecc74b760fc0a3f
waynewu6250 <waynewu6250@gmail.com>
2019-10-28 21:18:48 -0400

    Add

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 class Config:
 
     data_path = "data/raw_data.pkl" #"Multi-intent-dialoguer/data/raw_data.pkl"
+    dic_path = "data/intent2id.pkl"
     model_path = None #"bert_test.pth"
 
-    maxlen = 256
+    maxlen = 20
     batch_size = 16
     epochs = 25
     learning_rate_bert = 1e-3
