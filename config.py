21c28b1bea4f99929cfd5bb8b2616aa4c93440d7
waynewu6250 <waynewu6250@gmail.com>
2020-04-24 21:20:33 -0400

    add

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class Config:
     #################### For Clustering & DCEC ####################
     
     dic_path = "/nethome/twu367/Multi-intent-dialoguer/data/semantic/intent2id_se.pkl"
-    embedding_path = "/nethome/twu367/Multi-intent-dialoguer/finetune_results/se_embeddings_with_hidden.pth"
+    embedding_path = "/nethome/twu367/Multi-intent-dialoguer/finetune_results/se_embeddings_raw_with_hidden.pth"
     woz_dic_path = "/nethome/twu367/Multi-intent-dialoguer/data/MULTIWOZ2.1/intent2id.pkl"
     woz_embedding_path = "/nethome/twu367/Multi-intent-dialoguer/finetune_results/woz_embeddings_sub.pth"
     
